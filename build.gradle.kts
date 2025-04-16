80afce3928edfafba44ed6590fff1bb6f6bf2606
Esoteric Enderman <esotericenderman@gmail.com>
2025-04-16 18:26:32 +0100

    Clean up build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,64 +10,23 @@ plugins {
   id("xyz.jpenilla.run-paper") version "2.3.0"
 }
 
-val groupStringSeparator = "."
-val kebabcaseStringSeparator = "-"
-val snakecaseStringSeparator = "_"
-
-fun capitalizeFirstLetter(string: String): String {
-  return string.first().uppercase() + string.slice(IntRange(1, string.length - 1))
-}
-
-fun snakecase(kebabcaseString: String): String {
-  return kebabcaseString.lowercase().replace(kebabcaseStringSeparator, snakecaseStringSeparator)
-}
-
-fun pascalcase(kebabcaseString: String): String {
-  var pascalCaseString = ""
-
-  val splitString = kebabcaseString.split(kebabcaseStringSeparator)
-
-  for (part in splitString) {
-    pascalCaseString += capitalizeFirstLetter(part)
-  }
-
-  return pascalCaseString
-}
-
 description = "A very simple cosmetics plugin featuring hats and trails!"
-
-val mainProjectAuthor = "Esoteric Enderman"
-val projectAuthors = listOfNotNull(mainProjectAuthor)
-
-val topLevelDomain = "dev"
-
-val projectNameString = rootProject.name
 
 group = "dev.enderman"
 version = "1.0.0"
 
-val buildDirectoryString = buildDir.toString()
-
-val projectGroupString = group.toString()
-val projectVersionString = version.toString()
-
 val javaVersion = 21
-val javaVersionEnumMember = JavaVersion.valueOf("VERSION_" + javaVersion)
-val paperApiVersion = "1.21"
+val javaVersionEnum = JavaVersion.valueOf("VERSION_$javaVersion")
 
 java {
-  sourceCompatibility = javaVersionEnumMember
-  targetCompatibility = javaVersionEnumMember
+  sourceCompatibility = javaVersionEnum
+  targetCompatibility = javaVersionEnum
 
   toolchain.languageVersion = JavaLanguageVersion.of(javaVersion)
 }
 
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-  paperweight.paperDevBundle(paperApiVersion + "-R0.1-SNAPSHOT")
+  paperweight.paperDevBundle("1.21-R0.1-SNAPSHOT")
 }
 
 tasks {
